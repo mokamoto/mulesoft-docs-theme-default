b5bbc3b62160d73d6fa199089a2141c927fdfc6a
Dan Allen <dan.j.allen@gmail.com>
2017-09-01 21:19:54 -0600

    bring fonts into project to better control them

--- tasks/build.js
+++ tasks/build.js
@@ -26,12 +26,12 @@ const postcssPlugins = [
   postcssCalc(),
   autoprefixer({ browsers: ['last 2 versions'] }),
   postcssUrl({
-    url: function (asset) {
-      if (asset.pathname && minimatch(asset.pathname, './files/*.{svg,eot,woff,woff2}')) {
-        const parsedPath = path.parse(asset.pathname)
-        return path.join('..', 'fonts', parsedPath.base)
-      }
-    },
+    //url: function (asset) {
+    //  if (asset.pathname && minimatch(asset.pathname, './files/*.{svg,eot,woff,woff2}')) {
+    //    const parsedPath = path.parse(asset.pathname)
+    //    return path.join('..', 'fonts', parsedPath.base)
+    //  }
+    //},
   }),
   cssnano({ preset: 'default' }),
 ]
@@ -51,17 +51,17 @@ module.exports = (src, dest, cacheBuster) => {
 
     vfs.src('scripts/*.pack.js', srcOptions),
 
-    vfs.src('fonts/*.{svg,eot,woff,woff2}', srcOptions),
+    vfs.src('fonts/*.{woff,woff2}', srcOptions),
 
     vfs.src('stylesheets/theme.css', srcOptions)
       .pipe(postcss(postcssPlugins)),
 
-    vfs.src('node_modules/typeface-*/**/*.{svg,eot,woff,woff2}', srcOptions)
-      .pipe(map((file, next) => {
-        // move font files to fonts (without any subfolder)
-        file.dirname = path.join(file.base, 'fonts')
-        next(null, file)
-      })),
+    //vfs.src('node_modules/typeface-*/**/*.{svg,eot,woff,woff2}', srcOptions)
+    //  .pipe(map((file, next) => {
+    //    // move font files to fonts (without any subfolder)
+    //    file.dirname = path.join(file.base, 'fonts')
+    //    next(null, file)
+    //  })),
 
     vfs.src('helpers/*.js', srcOptions),
     vfs.src('layouts/*.hbs', srcOptions)
