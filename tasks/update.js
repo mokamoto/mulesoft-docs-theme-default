4f567825a338853371833b3b1c383779f4f1ee4b
runnabro <tli415@gmail.com>
2018-07-17 17:59:43 -0700

    update marketing update script

--- tasks/update.js
+++ tasks/update.js
@@ -4,23 +4,51 @@ const merge = require('merge-stream')
 const download = require('gulp-download-stream')
 const buffer = require('vinyl-buffer')
 const replace = require('gulp-replace')
+const https = require('https');
+const fs = require('fs');
 
+const headerPath = 'https://www.mulesoft.com/api/header';
+const headerFileName = 'header-shared.hbs';
+const footerPath = 'https://www.mulesoft.com/api/footer';
+const footerFileName = 'footer-shared.hbs';
+const dependencies = 'https://www.mulesoft.com/api/dependencies';
 
-function assetDownload(url, filename, destination){
-  return download({
-          url: url,
-          file: filename
-  }).pipe(buffer()).pipe(vfs.dest(destination))
-}
+function getHbs(url, fileName){
+  https.get(url, function(response){
+    let body = '';
+    response.on('data', function(d) {
+      body += d;
+    });
+    response.on('end', function() {
+      const parsed = JSON.parse(body).data;
+      fs.writeFileSync('partials/' + fileName, parsed);
+    });
+  });
+};
+
+function getDependencies(url, type){
+  https.get(url, function(response){
+    let body = '';
+    response.on('data', function(d) {
+      body += d;
+    });
+    response.on('end', function() {
+      const parsed = JSON.parse(body).data;
+      let styleLink = '';
+      let scriptLink = '';
+      for (let i = 0; i < parsed.styles.length; i++) {
+        styleLink += `<link rel="stylesheet" href="${parsed.styles[i]}" type="text/css">`;
+      }
+      for (let i = 0; i < parsed.scripts.length; i++) {
+        scriptLink += `<script src="${parsed.scripts[i]}"></script>`;
+      }
+      fs.writeFileSync('partials/head-shared.hbs', styleLink + scriptLink);
+    });
+  });
+};
 
 module.exports = () => {
-  return merge([
-      // markup
-      assetDownload('https://mulesoft.com/markup/get/header?searchbox=false', 'header-shared.hbs', 'partials'),
-      assetDownload('https://mulesoft.com/markup/get/footer', 'footer-shared.hbs', 'partials'),
-      // scripts
-      assetDownload('https://code.jquery.com/jquery-1.12.4.min.js', '04-jquery-1.12.4.min.js', 'scripts'),
-      assetDownload('https://app-abd.marketo.com/js/forms2/js/forms2.min.js', '06-form2.min.js', 'scripts'),
-      assetDownload('https://www.mulesoft.com/sites/all/themes/muletheme/js/header-footer.js', '05-header-footer.js', 'scripts'),
-  ])
+  getHbs(headerPath, headerFileName);
+  getHbs(footerPath, footerFileName);
+  getDependencies(dependencies);
 }
